7bdf00e2c2bd8ae1b077ef712a1d1172c4847f6c
Jon Cohen <jon.cohen08@gmail.com>
2012-06-30 16:45:46 -0400

    Started Back-end stuff

--- sign_up.php
+++ sign_up.php
@@ -1,9 +1,19 @@
+<?php 
+	$name = $_POST['user_name'];
+	$pw = $_POST['password']
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <title>Jonathan S. Cohen - Photography, Design and Development</title>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="css/styles.css" />
+<script>
+
+
+
+</script>
 </head>
 <body>
 <div id="wrapper">
@@ -21,8 +31,13 @@
 
 <div class="break"></div>
 
-<br />
-<br />
+<h3>Please enter your email or a username and a password</h3>
+
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<label>Username/Email</label><input type="text" name="user_name" maxlength="15" >
+<label>Password</label><input type="password" name="password">
+<input type="submit" value="submit" name="submit" onClick="checkForm()">
+</form>
 
 <footer>
 <div><a href="index.html">Home</a> | <a href="photos.html">Photography</a></div>
