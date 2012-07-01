e3d42b42cf873e2faa8a10f91fefa4186e6cc6e9
Jon Cohen <jon.cohen08@gmail.com>
2012-07-01 09:46:39 -0400

    Validated markup, changed photo page, created sign up page

--- sign_up.php
+++ sign_up.php
@@ -7,22 +7,57 @@
 <html lang="en">
 <head>
 <title>Jonathan S. Cohen - Photography, Design and Development</title>
-<meta charset="utf-8">
+<meta charset="utf-8" />
 <link rel="stylesheet" type="text/css" href="css/styles.css" />
+<script src="js/jquery-1.7.2.min.js"></script>
 <script>
 
-
+function checkForm () {
+	var ck_name = /^[A-Za-z]{3,8}$/;
+	var ck_email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	var ck_pw = /^[a-z0-9_-]{6,18}$/;
+	if ((document.signup.user_name.value.search(ck_name)==-1) && (document.signup.user_name.value.search(ck_email)==-1)) {
+		alert("Please enter a valid user name or email address");
+	}
+	if (document.signup.password.value.search(ck_pw)==-1) {
+		alert("Please enter a valid password");
+	}
+};
 
 </script>
+<style>
+	h3 {
+		text-align:center;
+	}
+	#form {
+		width:300px;
+		overflow:hidden;
+        padding:10px;
+		margin: 0 auto;
+	}
+	
+	label, input {
+		float:left;
+	}
+	
+	label {
+		display:block;
+		clear:left;
+		width:125px;
+	}
+	#submit {
+		margin-left:100px;
+	}
+</style>
 </head>
 <body>
-<div id="wrapper">
+<div id="body_wrapper">
 <header>
 <h1>Jonathan S. Cohen</h1>
 <h2 class="tagline">Photography, Design and Development</h2>
 
 <nav>
-  <ul>
+  <ul id="nav">
     <li><a href="index.html" class="bordered">Home</a></li>
     <li><a href="photos.html" class="bordered">Photography</a></li>
   </ul>
@@ -31,13 +66,17 @@
 
 <div class="break"></div>
 
-<h3>Please enter your email or a username and a password</h3>
+<h3>Please enter your email or a username from 3 to 8 characters and a password.  Your password must be at least 6 characters and less than 18.</h3>
 
-<form method="post" action="<?php echo $PHP_SELF;?>">
-<label>Username/Email</label><input type="text" name="user_name" maxlength="15" >
-<label>Password</label><input type="password" name="password">
-<input type="submit" value="submit" name="submit" onClick="checkForm()">
+<div id="form">
+<form method="post" name="signup">
+<label>Username/Email</label>
+<input type="text" name="user_name" /><br />
+<label>Password</label>
+<input type="password" name="password" id="form2" /><br /><br />
+<input type="submit" value="Submit" name="submit" id="submit" onClick="checkForm()" />
 </form>
+</div>
 
 <footer>
 <div><a href="index.html">Home</a> | <a href="photos.html">Photography</a></div>
