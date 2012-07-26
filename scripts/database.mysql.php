37e2622ac05837d05a4e7be80bfbd85338402e85
Jon Cohen <jon.cohen08@gmail.com>
2012-07-26 09:34:36 -0400

    changed db pw

--- scripts/database.mysql.php
+++ scripts/database.mysql.php
@@ -1,6 +1,6 @@
 <?php
 
-$resource = mysql_connect("localhost", "root", "") or die("Unable to connect" . mysql_error());
+$resource = mysql_connect("localhost", "root", "bleddavy") or die("Unable to connect" . mysql_error());
 
 mysql_select_db("photo_store", $resource) or die("Unable to connect to database");
 
