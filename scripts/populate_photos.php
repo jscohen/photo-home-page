7799b53ed65ab6bf7aa1b844e805671e4963a7d7
Jon Cohen <jon.cohen08@gmail.com>
2012-07-31 20:35:23 -0400

    Improved database connection

---
+++ scripts/populate_photos.php
@@ -0,0 +1,27 @@
+<?php 
+
+session_start();
+
+require "database.mysql.php";
+
+$costs = array();
+$descs = array();
+
+$prices = mysql_query("SELECT price FROM photos", $resource);
+$descriptions = mysql_query("SELECT description FROM photos", $resource);
+
+while($price = mysql_fetch_array($prices)) {
+	array_push($costs,$price['price']);
+}
+
+while($desc = mysql_fetch_array($descriptions)) {
+	array_push($descs,$desc['description']);
+}
+
+$_SESSION['prices'] = $costs;
+$_SESSION['descriptions'] = $descs;
+
+print_r($_SESSION['prices']);
+print_r($_SESSION['descriptions']);
+
+?>
